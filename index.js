98279e1b63bb48de60e75ff31418423dbb1d6e3d
Vladuslav Kondratskyi <55400806+1Mayuko1@users.noreply.github.com>
2022-09-20 17:37:50 +0300

    findIndices done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+// find indices of two numbers in a list that add up to a target number
+// list: [2, 7, 11, 15], target: 9
+
+let target = 9
+let list = [11, 2, 7, 15]
+let res = []
+
+const findIndices = (target, list) => {
+    list.map((itemX, indexX) => {
+        list.map((itemY, indexY) => {
+            if (itemX + itemY === target) {
+                res.push(indexX, indexY)
+            } else {
+                return []
+            }
+        })
+    })
+    let array = [...new Set(res)]
+    return array.length === 0 ? [] : [list[array[0]], list[array[1]]]
+}
+
+console.log(findIndices(target, list))
